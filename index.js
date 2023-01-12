220f08444958100b7004987e86be3c73d1831344
sabiha22 <54326183+sabiha22@users.noreply.github.com>
2023-01-12 12:08:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
+const billAmount= document.querySelector("#bill-amount");
+const cashGiven= document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes= document.querySelectorAll(".no-of-notes");
 
 const availableNotes= [2000,500,100,20,10,5,1];
 
+
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
     hideMessage();
-    console.log(billAmount.value);
-    if(billAmount.value>0) {
-        if(cashGiven.value>= billAmount.value){
-            const amountToBeReturned= cashGiven.value- billAmount.value;
+    console.log(billAmount);
+    if(Number(billAmount.value)>0) {
+        if(Number(cashGiven.value)>= Number(billAmount.value)){
+            const amountToBeReturned= Number(cashGiven.value)- Number(billAmount.value);
             calculateChange(amountToBeReturned);
         }
         else{
